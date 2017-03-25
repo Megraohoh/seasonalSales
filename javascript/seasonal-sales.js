581b036baaeffb868f13182faa8a103ab3c96a6e
Meghan Debity <mdebity@gmail.com>
2017-03-25 12:24:50 -0500

    cleaning it up

--- javascript/seasonal-sales.js
+++ javascript/seasonal-sales.js
@@ -2,13 +2,12 @@ var productsContainer = document.getElementById("productsContainer");
 var departmentsContainer = document.getElementById("departmentsContainer");
 var productArray = [];
 var departmentArray = [];
-
+var productsString = "";
 ///////////////////////////////////////////PRODUCTS
 
-function sendProductsToDom(xhrData){
-	// var productsString = "";
-
-
+function dataHandler(data){
+	
+	products = data.products;
 	productArray.forEach(function(products){
 		for (var i=0; i<departmentArray.length; i++) {
 			console.log(products.category_id);
@@ -21,53 +20,70 @@ function sendProductsToDom(xhrData){
 			}
 		}
 	})
-	console.log(productArray);
+	writeToDom();
 };
 
-function executeSalesAfterFileIsLoaded(){
+function writeToDom(season){
+	var productBuilder ="";
+	for (var i = 0; i < productArray.length; i++) {
+		productBuilder += `<div class="row">`
+		productBuilder += `<div class="col-sm-9 col-md-6"><h3>${products[i].name}</h3>`
+		if (season === productArray[i].categorySeasonDiscount) {
+			productBuilder += `<div class="row"> <div class="col-xs-8 col-sm-6"><h4>${products[i]["season-price"]}</h4></div>`
+		} else {
+			productBuilder += `<div class="row"> <div class="col-xs-8 col-sm-6"><h4>${products[i].price}</h4></div>`
+
+		}
+		productBuilder += `<div class="col-xs-4 col-sm-6"><h4>${products[i].categorySeasonDiscount}</h4></div>`
+		productBuilder += `</div></div></div>`
+		productsContainer.innerHTML = productBuilder
+
+}
+
+var dropDown = document.getElementById("chooseSeason");
+
+dropDown.addEventListener("change", function(e){
+    var seasonSelection = e.target.value;
+    writeToDom(seasonSelection);
+});
+
+
+function executeParseProducts(){
 	var data = JSON.parse(this.responseText);
 
 		for(var i=0; i<data.products.length; i++){
 		productArray.push(data.products[i])
 	}
-	console.log(productArray);
-sendProductsToDom(data);
-};
+	// console.log(productArray);
+dataHandler(data);
+}
 
-
+function executeDepartmentsAfterFileIsLoaded(){
+	var data = JSON.parse(this.responseText);
+	// console.log(data);
+	}
+}
 
 function executeThisCodeAfterFileFails(){
 	alert("Sale is Unavailable");
 }
 
 
-/////////////////////////////////////////DEPARTMENTS
-
-function executeDepartmentsAfterFileIsLoaded(){
-	var data = JSON.parse(this.responseText);
-	console.log(data);
-	// sendDepartmentsToDom(data);
-
-		for(var i=0; i<data.categories.length; i++){
-		departmentArray.push(data.categories[i])
-}
-
-	var myRequestProducts = new XMLHttpRequest();
-	myRequestProducts.addEventListener("load", executeSalesAfterFileIsLoaded);
-	myRequestProducts.addEventListener("error", executeThisCodeAfterFileFails);
-	myRequestProducts.open("GET", "products.json");
-	myRequestProducts.send();
 
 
-}
-
-function executeThisCodeAfterFileFails(){
-	alert("Departments are Unavailable");
-}
+var myRequestProducts = new XMLHttpRequest();
+myRequestProducts.addEventListener("load", executeParseProducts);
+myRequestProducts.addEventListener("error", executeThisCodeAfterFileFails);
+myRequestProducts.open("GET", "products.json");
+myRequestProducts.send();
 
 var myRequestDepartments = new XMLHttpRequest();
 myRequestDepartments.addEventListener("load", executeDepartmentsAfterFileIsLoaded);
 myRequestDepartments.addEventListener("error", executeThisCodeAfterFileFails);
 myRequestDepartments.open("GET", "departments.json");
 myRequestDepartments.send();
+
+
+
+
 
